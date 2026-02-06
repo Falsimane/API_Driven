289d59626e63e9ecbcd5036d49909f47f4a3349f
Falsimane <93453007+Falsimane@users.noreply.github.com>
2026-02-06 09:18:33 +0000

    MODIF: delete useless variables export

--- setup_env.sh
+++ setup_env.sh
@@ -5,9 +5,4 @@ echo "--- Installation des outils nécessaires ---"
 pip install awscli-local boto3
 sudo apt-get update && sudo apt-get install -y zip jq
 
-# Configuration des identifiants de test pour LocalStack
-export AWS_ACCESS_KEY_ID=test
-export AWS_SECRET_ACCESS_KEY=test
-export AWS_DEFAULT_REGION=us-east-1
-
 echo "Environnement prêt !" 
