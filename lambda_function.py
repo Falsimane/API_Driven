db7545825eac610acfe8933adb627798b9166d92
Falsimane <93453007+Falsimane@users.noreply.github.com>
2026-02-06 09:10:11 +0000

    add: python script for the handling of lambda function

---
+++ lambda_function.py
@@ -0,0 +1,51 @@
+import boto3
+import json 
+import os 
+
+ENDPOINT_URL = os.environ.get("ENDPOINT_URL", "https://literate-space-guide-pq46pgj677937wp7-4566.app.github.dev/")
+
+def lambda_handler(event, context): 
+    print(f"Connexion à l'endpoint : {ENDPOINT_URL}")
+
+    # Configuration du client 
+    ec2 = boto3.client("ec2", endpoint_url=ENDPOINT_URL, region_name="us-east-1")
+
+    # Analyse des paramètres
+    params = event.get("queryStringParameters" or {})
+    
+    action = params.get("action")
+    instance_id = params.get("instance_id")
+
+    if not action or not instance_id:
+        return{
+            "statusCode": 400,
+            "body": json.dumps("Erreur: Paramètres \"action\" et \"instance_id\" requis.")
+        }
+
+    try:
+        if action == "start":
+            ec2.start_instances(InstanceIds=[instance_id])
+            msg = f"Instance {instance_id} démarrée."
+        elif action == "stop":
+            ec2.stop_instances(InstanceIds=[instance_id])
+            msg = f"Instance {instance_id} arrêtée."
+        else:
+            return {
+                "statusCode": 400,
+                "body": json.dumps("Action invalide (start/stop) uniquement.")
+            }
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message":msg,
+                "endpoint": ENDPOINT_URL
+            })
+        }
+    except Exception as e:
+        return{
+            "statusCode": 500,
+            "body": json.dumps(f"Erreur AWS: {str(e)}")
+        }
+            
+
+          
